0d34ba1707bfe3c6207641292a207697390d6d67
Elis Souza <eldayane@gmail.com>
2020-05-10 19:01:37 -0300

    alteração de logout

--- ProjetoPraticoCodenation/Controllers/AuthController.cs
+++ ProjetoPraticoCodenation/Controllers/AuthController.cs
@@ -83,6 +83,7 @@ namespace ProjetoPraticoCodenation.Controllers
 
         [HttpPost("logout")]
         [ValidateAntiForgeryToken]
+        [AllowAnonymous]
         public async Task<IActionResult> Logout()
         {
             await _signInManager.SignOutAsync();
